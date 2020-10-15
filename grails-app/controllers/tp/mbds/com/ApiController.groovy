707d46eac18d673c369557fdc4e5b2c97584369d
zaza <victor.azalbert@yahoo.fr>
2020-10-15 15:22:04 +0200

    ok

--- grails-app/controllers/tp/mbds/com/ApiController.groovy
+++ grails-app/controllers/tp/mbds/com/ApiController.groovy
@@ -21,6 +21,13 @@ class ApiController {
                }
            break
            case "PUT":
+               // HTTP 201 (CREATED) HTTP 204 (NO CONTENT) HTTP 404 (NOT FOUND) HTTP 400 (BAD REQUEST)
+
+
+               response.withFormat {
+                   xml { render userInstance as XML }
+                   json { render userInstance as JSON }
+               }
            break
            case "PATCH":
            break
@@ -35,7 +42,34 @@ class ApiController {
 
     def Users(){ }
 
-    def SaleAd(){ }
+    def SaleAd(){
+        switch(request.getMethod()){
+            case "GET":
+                if (!params.id)
+                    return response.status = 400
+                def saleAdInstance = SaleAd.get(params.id)
+                if (!saleAdInstance)
+
+                    return response.status = 404
+                response.withFormat {
+                    xml { render saleAdInstance as XML }
+                    json { render saleAdInstance as JSON }
+                }
+                break
+            case "PUT":
+                break
+            case "PATCH":
+                break
+            case "DELETE":
+                break
+            default:
+                return response.status = 405
+                break
+        }
+        return response.status  = 406
+
+
+    }
 
     def SaleAds(){ }
 }
